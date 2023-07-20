ddff5be1fcc221a7657d65a9d4e70c14a4a24c1f
= <=>
2023-07-20 19:31:30 +0700

    AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,20 @@ Route::resource('Activities',ActivitiesController::class);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/a', function () {
+    return view('master');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::view('about', 'about')->name('about');
+
+    Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+
+    Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
+    Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+});
