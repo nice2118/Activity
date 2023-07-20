b6f0f684033d64a9ab92464bf1ab944b7855ce7b
= <=>
2023-07-20 22:39:06 +0700

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('content')
+@section('content2')
     <!-- Content Header (Page header) -->
     <div class="content-header">
         <div class="container-fluid">
